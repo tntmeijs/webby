a8994d098bb317fc34c744db7b12fd3c98e83351
Tahar <tntmeijs@gmail.com>
2021-04-27 10:35:23 +0200

    Bind a TCP listener and listen for connections

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,3 +1,11 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    for stream in listener.incoming() {
+        stream.unwrap();
+
+        println!("Connection received!");
+    }
 }
