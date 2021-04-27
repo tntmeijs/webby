0363511b747343def88e6ac388f1af952fde0013
Tahar <tntmeijs@gmail.com>
2021-04-27 10:57:26 +0200

    Read stream into buffer

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,11 +1,16 @@
-use std::net::TcpListener;
+use std::{io::Read, net::{TcpListener, TcpStream}};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
 
     for stream in listener.incoming() {
-        stream.unwrap();
-
-        println!("Connection received!");
+        handle_connection(&mut stream.unwrap());
     }
+}
+
+fn handle_connection(stream: &mut TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
