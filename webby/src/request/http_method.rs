c0d696f49ed7d216d0d013de06ec87e16dc2c1a9
Tahar <tntmeijs@gmail.com>
2021-05-02 13:33:33 +0200

    PartialEq, Eq, and from implementation for HTTP method enum

--- webby/src/request/http_method.rs
+++ webby/src/request/http_method.rs
@@ -1,4 +1,6 @@
+#[derive(PartialEq, Eq)]
 pub enum HttpMethod {
+    INVALID,
     GET,
     HEAD,
     POST,
@@ -7,5 +9,22 @@ pub enum HttpMethod {
     CONNECT,
     OPTIONS,
     TRACE,
-    PATCH
+    PATCH,
+}
+
+impl From<&str> for HttpMethod {
+    fn from(str: &str) -> Self {
+        match str.trim() {
+            "GET" => Self::GET,
+            "HEAD" => Self::HEAD,
+            "POST" => Self::POST,
+            "PUT" => Self::PUT,
+            "DELETE" => Self::DELETE,
+            "CONNECT" => Self::CONNECT,
+            "OPTIONS" => Self::OPTIONS,
+            "TRACE" => Self::TRACE,
+            "PATCH" => Self::PATCH,
+            _ => Self::INVALID,
+        }
+    }
 }
